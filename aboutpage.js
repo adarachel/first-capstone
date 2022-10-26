9a4906ab6c72360b423a197d00f0badde8bcf556
adarachel <108772486+adarachel@users.noreply.github.com>
2022-10-26 08:52:17 +0100

    Capstone Project

---
+++ aboutpage.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById('pageNav').style.width = '100%';
+  }
+  
+  function closeNav() {
+    document.getElementById('pageNav').style.width = '0%';
+  }
+  
+  openNav();
+  closeNav();
